88a9756b6fb888e3bbf8bfd7d431280dec0cc734
Adrian Mrowka <varnaxes0@gmail.com>
2018-09-02 20:45:43 +0200

    Now it works for a short quotes
    but no longer than 40 characters

--- main.cpp
+++ main.cpp
@@ -4,25 +4,49 @@
 #include <iostream>
 #include <string>
 
-void print_text();
+void print_text(std::string);
 void print_cow();
+bool is_short(std::string);
+void print_upper_frame();
+void print_line(std::string, int);
+void print_lower_frame();
 
 int main()
 {
     std::string text;
 
-    std::cout << "Paste a quote: ";
+    std::cout << "Type a quote: ";
     getline(std::cin, text);
 
     //test:
     std::cout << text << "\n\n";
 
-    print_text();
+    print_text(text);
     print_cow();
 }
 
-void print_text()
+void print_text(std::string text)
 {
+    print_upper_frame();
+
+    if (is_short(text))
+    {
+        int spaces = 40 - text.length();
+        print_line(text, spaces);
+        print_lower_frame();
+        return;
+    }
+/*
+    std::string::iterator it;
+    std::string line = "";
+    int i = 0;
+
+    for(it = text.begin(); (it != text.end()) && (i < 40); ++it)
+    {
+        line.push_back(*it);
+        ++i;
+    }
+*/
     //print upper frame
     //check length of the quote, if it has less than 40 characters -> print whole
         //else: print up to first space before 40th character + add ' ' to fill rest of the line
@@ -34,3 +58,40 @@ void print_cow()
 {
     //just print ascii "cow"
 }
+
+void print_upper_frame()
+{
+    char h = 205, sw = 187, se = 201;
+    std::cout << se;
+    for (int i = 0; i < 40; ++i)
+        std::cout << h; 
+    std::cout << sw << '\n';
+}
+
+void print_line(std::string text, int spaces)
+{
+    char v = 186;
+    std::cout << v << text;
+    for (int i = 0; i < spaces; ++i)
+        std::cout << ' ';
+    std::cout << v << '\n';
+}
+
+void print_lower_frame()
+{
+    char v = 186, h = 205, nw = 188, ne = 200;
+    std::cout << v << ' ';
+    for (int i = 0; i < 39; ++i)
+        std::cout << h; 
+    std::cout << nw << '\n';
+    std::cout << v << "/\n";
+}
+
+bool is_short(std::string text)
+{
+    if (text.size() > 40)
+        return false;
+    else
+        return true;
+}
+
